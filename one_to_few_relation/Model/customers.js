cb0b676e31a902c7c002713155e1766a7c846ee3
123Umair <khanuumair91@gmail.com>
2026-03-31 11:57:21 +0500

    one to many relationship schema

---
+++ one_to_few_relation/Model/customers.js
@@ -0,0 +1,59 @@
+import mongoose, { Schema , Model } from "mongoose";
+
+
+
+const main = async() =>{
+     await mongoose.connect('mongodb://127.0.0.1:27017/mongoose_relatoins');
+   }
+main().then(()=>{
+    console.log("Db Connected")
+})
+
+
+
+const userOrderSchema = new Schema({
+    name:String,
+    price:Number,
+})
+
+
+const OrderModel = mongoose.model("Order",userOrderSchema)
+
+// const addOrder = async() =>{
+//     const newOrder = await OrderModel.insertMany([
+//         {name:"Pizza",price:1600},
+//         {name:"Aloo Chips",price:500},
+//         {name:"Samosa",price:40},
+//         {name:"Zingerburger",price:500}
+//     ])
+//     console.log(newOrder)
+// }
+
+// addOrder()
+
+
+
+const customerSchema = new Schema({
+    name:String,
+    order:[
+        {type:Schema.Types.ObjectId, //store object id  only (not all the data) in the mongodb but mongoose shows it full data but in themongo db its store only the id 
+         ref:"OrderModel" // inside ref use the model name where you u stored the actual ids of the orders.
+        }
+    ]
+})
+
+const CustomerModel  = mongoose.model("Customer",customerSchema)
+
+const addCustomer=async()=>{
+    const cust1 = new CustomerModel({
+        name:"Umair"
+    })
+    let order1 = await OrderModel.findOne({name:"Pizza"})
+    let order2 = await OrderModel.findOne({name:"Samosa"})
+    cust1.order.push(order1)
+    cust1.order.push(order2)
+    let result = await cust1.save()
+    console.log(result)
+}
+
+addCustomer()
