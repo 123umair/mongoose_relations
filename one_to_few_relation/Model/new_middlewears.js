c2fdc61751ca041a505b7ea60f9155dbf9f33e01
123Umair <khanuumair91@gmail.com>
2026-04-18 16:11:53 +0500

    Using the middleWear post of mongoose

--- one_to_few_relation/Model/new_middlewears.js
+++ one_to_few_relation/Model/new_middlewears.js
@@ -24,7 +24,7 @@ const orderModel = mongoose.model("NewOrder",orderSchema)
 //     await orders.save()
 // }
 
-// addOrder()
+// // addOrder()
 const customerSchema = new Schema({
     name:String,
     order:[
@@ -33,25 +33,32 @@ const customerSchema = new Schema({
 
     ]
 })  
+customerSchema.post("findOneAndDelete",async(customers)=>{
+   if (customers.order.length){
+        await orderModel.deleteMany({_id:{$in:customers.order}})  
+    }
+})
 const customerModel = mongoose.model("NewCustomer",customerSchema)
 
 // const addcustomerorder = async() =>{
-//     const add = new customerModel({name:"umairkhan",})
+// // // //     const add = new customerModel({name:"umairkhan",})
 //     const addcust2 = new customerModel({name:"Sajid khan"})
-//     let order1 = await orderModel.findOne({ ordername:"Pizzaa",address:"khayaban chow"})
-//     let order2 = await orderModel.insertOne({ordername:"Shorma",address:"town"})
+// // // //     let order1 = await orderModel.findOne({ ordername:"Pizzaa",address:"khayaban chow"})
+//     let order2 = await orderModel.insertOne({ordername:"burgur",address:"ustb"})
 //     addcust2.order.push(order2)
-//     add.order.push(order1,order2)
-//     await add.save()
+//     // add.order.push(order1,order2)
+// // // //     await add.save()
 //     await addcust2.save()
 //     console.log('data is successfully inseted')
 // }
 // addcustomerorder()
 
+// now we use the post middlewares of the mongoose that will deleted the related data of a customer model means its orders after delted the customer.
 
 const delCust = async()=>{
-    const customersajid = await customerModel.findByIdAndDelete("69e33698cfbdba6bd1a2222f")
-    // await customersajid.save()
+    const customersajid = await customerModel.findByIdAndDelete("69e363ed929849ca599cc837")
+   
     console.log(customersajid,'customersajid')   // so here we conclude that only the customer is deleted name sajid document in the model but not its order its will remain present in the ordermodel but its related the order with sajid so basically its happenning in normal case.
 } 
 delCust()
+
