ba47bc25f9db2d8583a469df5cdfeb129d7b02d0
123Umair <khanuumair91@gmail.com>
2026-04-22 22:30:33 +0500

    Reading and Accessing Cookies

--- one_to_few_relation/Model/cookies.js
+++ one_to_few_relation/Model/cookies.js
@@ -2,13 +2,18 @@
 
 import express from 'express'
 const app  = express()
+import cookieparser from 'cookie-parser'
+app.use(cookieparser())
 
-app.use("/",(req,res,)=>{
+app.get("/cookie",(req,res,)=>{
     res.cookie('Name',"umair") // here we can pass two parameters in the cookie.send(name,value) the first one is name and the 2nd one is value
     res.cookie('created_by','umairkhan')
     res.send('send you some cookies')
 })
-
+app.get('/',(req,res)=>{
+    console.dir(req.cookies)
+    res.send(req.cookies)
+})
 app.listen(4000,()=>{
     console.log('server is start')
 })
