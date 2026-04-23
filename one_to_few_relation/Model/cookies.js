2a5686ba74a3f47229af5a84776cb0402228a775
123Umair <khanuumair91@gmail.com>
2026-04-23 07:59:22 +0500

    Signed Cookies

--- one_to_few_relation/Model/cookies.js
+++ one_to_few_relation/Model/cookies.js
@@ -3,7 +3,13 @@
 import express from 'express'
 const app  = express()
 import cookieparser from 'cookie-parser'
-app.use(cookieparser())
+app.use(cookieparser("umair")) //signature
+
+
+app.get('/signedcookie',(req,res)=>{
+    res.cookie('name','sajid',{signed:true})
+    res.send(req.signedCookies) // singesCookies data will shows as a req.singedCookies
+})
 
 app.get("/cookie",(req,res,)=>{
     res.cookie('Name',"umair") // here we can pass two parameters in the cookie.send(name,value) the first one is name and the 2nd one is value
@@ -17,3 +23,6 @@ app.get('/',(req,res)=>{
 app.listen(4000,()=>{
     console.log('server is start')
 })
+
+// singed cookies is that type of the cookies that will prevent from unintentional changes from any other client.They prevent users from modifying cookie data, 
+
