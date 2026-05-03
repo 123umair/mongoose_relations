fab182ed4a43ac1fedf8cf7465a1059f0f2c02b1
123Umair <khanuumair91@gmail.com>
2026-05-03 21:22:57 +0500

    Learning Storing information in the store.

--- one_to_few_relation/Model/session.js
+++ one_to_few_relation/Model/session.js
@@ -1,4 +1,4 @@
-// so today now we are learing the sessions and make the statefull the requests of the http using the express sessions that will save the state by the server and the session information
+// so today now we are learning the sessions and make the statefull the requests of the http using the express sessions that will save the state by the server and the session information.
 import express from 'express'
 
 const app = express()
@@ -10,6 +10,41 @@ app.use(session({secret:'Mysecretstring'}))
 app.get('/test',(req,res)=>{
     res.send('Session testing')
 })
+
+// Express session storing and using info. example 
+
+// app.use(session({
+//   secret: 'your-secret-key', // Required for signing the session ID cookie
+//   resave: false,             // Only save if session is modified
+//   saveUninitialized: true    // Save new sessions even if they aren't modified
+// }));
+
+// purpose of usage:
+// New Visitors: A new session will be created and saved in the store immediately, and a cookie will be sent to their browser, even if they don't log in (saveUninitialized: true).
+// Returning Visitors: If a returning visitor makes a request but their session data isn't changed (e.g., they just view a page), the session store will not be updated with a "save" command, saving server resources (resave: false)
+
+const sessionOptions={
+    secret:"mySecretString",
+    resave:false,
+    saveUninitialized:true
+}
+app.use(session(sessionOptions))
+app.get('/register',(req,res)=>{
+   let { name } = req.query  // here i will use the query string first i destructure the name and stored it in the session store req.session
+   req.session.name = name
+   res.send(name)
+})
+
+
+
+
+
+
+
+
+
+
+
 app.listen(4000,()=>{
     console.log('Listening the request')
 })
